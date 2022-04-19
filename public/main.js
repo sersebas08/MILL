1095fb635d390c2214eed6db6bb616dcb41cac96
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2022-04-18 22:24:44 -0300

    refactor

--- public/main.js
+++ public/main.js
@@ -4,7 +4,6 @@ desayuno.onclick = cambio;
 let sentido = 0;
 function cambio (){
     const menu = document.getElementById('menu');
-
     if (sentido === 0) {
         menu.style.display = 'flex';
         sentido = 1;
