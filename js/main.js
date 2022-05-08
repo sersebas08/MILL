17dab3c6fc4092730ea5cf75f0ba8b80faf97498
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2022-05-08 16:47:45 -0300

    pruebas de interafce

--- js/main.js
+++ js/main.js
@@ -15,4 +15,56 @@ function cambio (){
 
 
 }*/
+document.addEventListener('DOMContentLoaded', (e) => {
+    log();
+
+})
+
+
+function log () {
+    /*const { latitude, longitude } = position.coords;*/
+
+   fetch('productos.json')
+        .then(response => response.json())
+        .then(data => {
+           data.forEach(data => {
+              leerProductos(data);
+           })
+           /*leerProductos(data);*/
+           console.log(data)
+        })
+        .catch(function (err){console.log("este es el error", err);})
+}
+
+
+
+function leerProductos(data){
+   let dataAppi = {
+      nombre: data.title,
+      intro: data.intro
+   }
+   console.log(dataAppi.nombre);
+    const leerJson = document.querySelector('.leerJson');
+    leerJson.innerHTML += `<div class="divProductos">
+                                <div class="w-3/6">
+                                    <picture class="flex m-auto">
+                                        <source media="(min-width: 751px)" srcset="img/cafe-con-leche.jpg">
+                                        <source media="(min-width: 380px)" srcset="img/cafe-con-leche.jpg">
+                                        <img src="img/cafe-con-leche.jpg" class="w-full h-32 " alt="Mi imagen responsive">
+                                    </picture>
+                                </div>
+                                <div class="w-3/6">
+                                    <h3 class="m-2">${dataAppi.nombre}</h3>
+                                    <p class="m-2">${dataAppi.intro}</p>
+                                </div>
+                            </div>`;
+
+}
+
+/*{
+   data.forEach(data => {
+      console.log(data)
+   })
+}*/
+/*console.log('este es el resultado: ', log);*/
 
