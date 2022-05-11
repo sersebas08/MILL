a0ff88d458d366e5cf87bbb9c5d7ee2b9472c6b9
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2022-05-11 11:25:49 -0300

    ventanaModal

--- js/main.js
+++ js/main.js
@@ -11,5 +11,29 @@ window.addEventListener('DOMContentLoaded', (e) => {
     platos();
 
 })
+const button = document.querySelector('.button');
+let ventanaHombres = document.querySelector('#ventanaHombres');
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    ventanaHombres.classList.add("modalHombre");
+    ventanaHombres.classList.remove("hidden");
+
+
+    let a = document.createElement('a');
+    a.href = '#';
+    a.id = 'modal'
+    a.innerHTML = 'X';
+    a.onclick = function () {
+
+        ventanaHombres.classList.add("hidden");
+        ventanaHombres.classList.remove("modalHombre");
+        return false;
+    }
+    ventanaHombres.appendChild(a);
+    console.log('hola mundo')
+
+})
+
 
 
