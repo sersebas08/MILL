03487ebfbf0a3002d20b751ab7e70722f9d93429
sebastian <85594721+sebaservente@users.noreply.github.com>
2022-05-15 15:10:57 -0300

    insertamos el a href

--- js/platos.js
+++ js/platos.js
@@ -20,18 +20,20 @@ export  function leerProductos(data){
     }
     console.log(dataAppi.nombre);
     const leerPlatos = document.querySelector('.leerPlatos');
-    leerPlatos.innerHTML += `<div class="divProductos drop-shadow-lg h-38">
-                                <div class="divProductos__img">
-                                    <picture class="flex m-auto">
-                                        <source media="(min-width: 751px)" srcset="${dataAppi.imgBig}">
-                                        <source media="(min-width: 380px)" srcset="${dataAppi.imgBig}">
-                                        <img src="${dataAppi.imgBig}" class="w-full" alt="Mi imagen responsive">
-                                    </picture>
-                                </div>
-                                <div class="divProductos__div">
-                                    <h3 class="divProductos__h3">${dataAppi.nombre}</h3>
-                                    <p class="divProductos__p"><span>Precio: $</span>${dataAppi.precio}</p>
-                                </div>
-                            </div>`;
+    leerPlatos.innerHTML += `    <a href="">
+                                    <div class="divProductos drop-shadow-lg h-38">
+                                        <div class="divProductos__img">
+                                            <picture class="flex m-auto">
+                                                <source media="(min-width: 751px)" srcset="${dataAppi.imgBig}">
+                                                <source media="(min-width: 380px)" srcset="${dataAppi.imgBig}">
+                                                <img src="${dataAppi.imgBig}" class="w-full" alt="Mi imagen responsive">
+                                            </picture>
+                                        </div>
+                                        <div class="divProductos__div">
+                                            <h3 class="divProductos__h3">${dataAppi.nombre}</h3>
+                                            <p class="divProductos__p"><span>Precio: $</span>${dataAppi.precio}</p>
+                                        </div>
+                                    </div>
+                                </a>  `;
 
 }
