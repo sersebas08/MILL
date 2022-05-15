bba54fdebe9a8e75e62fd69a4c2a071e8599b6af
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2022-05-15 20:42:54 -0300

    prog modal

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ import {bebidas} from "./bebidas.js";
 import {desayunosMeriendas} from "./desayunosMeriendas.js";
 import {platos} from "./platos.js";
 import {cafeteria} from "./cafeteria.js";
-import {leerProductos} from "./cafeteria.js";
+/*import {modal} from "./modal.js";*/
+
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
@@ -11,37 +12,11 @@ window.addEventListener('DOMContentLoaded', (e) => {
     cafeteria();
     bebidas();
     platos();
-
 })
 
 
 
-const button = document.querySelector('.button');
-let ventanaHombres = document.querySelector('#ventanaHombres');
-button.onclick = modal;
-/*button.addEventListener('click', (e) => {
-    e.preventDefault();*/
-function modal(){
 
-   /* console.log('esta es la data de cafeteria: ' , );*/
-    ventanaHombres.classList.add("modalHombre");
-    ventanaHombres.classList.remove("hidden");
-
-
-    let a = document.createElement('a');
-    a.href = '#';
-    a.id = 'modal'
-    a.innerHTML = 'X';
-    a.onclick = function () {
-
-        ventanaHombres.classList.add("hidden");
-        ventanaHombres.classList.remove("modalHombre");
-        return false;
-    }
-    ventanaHombres.appendChild(a);
-    console.log('hola mundo')
-
-}
 
 
 
--- js/platos.js
+++ js/platos.js
@@ -1,3 +1,5 @@
+
+
 export function platos () {
 
     fetch('./json/platos.json')
@@ -12,28 +14,66 @@ export function platos () {
         .catch(function (err){console.log("este es el error", err);})
 }
 
+/*button.addEventListener('click', (e) => {
+    e.preventDefault();*/
+export function modal(){
+    let ventanaHombres = document.querySelector('#ventanaHombres');
+    console.log('hasta llego');
+    /* console.log('esta es la data de cafeteria: ' , );*/
+    ventanaHombres.classList.add("modalHombre");
+    ventanaHombres.classList.remove("hidden");
+
+
+    let a = document.createElement('a');
+    a.href = '#';
+    a.id = 'modal'
+    a.innerHTML = 'X';
+    a.onclick = function () {
+
+        ventanaHombres.classList.add("hidden");
+        ventanaHombres.classList.remove("modalHombre");
+        return false;
+    }
+    ventanaHombres.appendChild(a);
+    console.log('hola mundo')
+
+}
+/*let button = document.querySelector('.button');
+button.style.backgroundColor = 'yellow';
+button.onclick = modal;*/
+
 export  function leerProductos(data){
-    let dataAppi = {
+
+
+    for (const prop in data) {
+        console.log(`obj.${prop} = ${data[prop]}`);
+
+        const leerPlatos = document.querySelector('.leerPlatos');
+        leerPlatos.innerHTML += ` <div class="divProductos drop-shadow-lg h-38">
+                                    <div class="divProductos__img">
+                                        <picture class="flex m-auto">
+                                            <source media="(min-width: 751px)" srcset="${data.imgBig}">
+                                            <source media="(min-width: 380px)" srcset="${data.imgBig}">
+                                            <img src="${data.imgBig}" class="w-full" alt="Mi imagen responsive">
+                                        </picture>
+                                    </div>
+                                    <div class="divProductos__div">
+                                        <h3 class="divProductos__h3">${data.title}</h3>
+                                        <p class="divProductos__p"><span>Precio: $</span>${data.precio}</p>
+                                    </div>
+                                </div>
+                                <button type="submit" class="button">ver mas</button>`;
+
+
+
+
+    }
+   /* let dataAppi = {
         nombre: data.title,
         precio: data.precio,
         imgBig: data.imgBig
-    }
-    console.log(dataAppi.nombre);
-    const leerPlatos = document.querySelector('.leerPlatos');
-    leerPlatos.innerHTML += `    <a href="#">
-                                    <div class="divProductos drop-shadow-lg h-38">
-                                        <div class="divProductos__img">
-                                            <picture class="flex m-auto">
-                                                <source media="(min-width: 751px)" srcset="${dataAppi.imgBig}">
-                                                <source media="(min-width: 380px)" srcset="${dataAppi.imgBig}">
-                                                <img src="${dataAppi.imgBig}" class="w-full" alt="Mi imagen responsive">
-                                            </picture>
-                                        </div>
-                                        <div class="divProductos__div">
-                                            <h3 class="divProductos__h3">${dataAppi.nombre}</h3>
-                                            <p class="divProductos__p"><span>Precio: $</span>${dataAppi.precio}</p>
-                                        </div>
-                                    </div>
-                                </a>  `;
+    }*/
+    /*console.log(prop.nombre);*/
+
 
 }
