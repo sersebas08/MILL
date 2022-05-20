4ac63ffe43e869504dbbe97253289bfb5a3b12a4
sersebas08 <57241783+sersebas08@users.noreply.github.com>
2022-05-20 01:19:35 -0300

    btn-v1

--- js/main.js
+++ js/main.js
@@ -6,7 +6,57 @@ import {cafeteria} from "./cafeteria.js";
 import {ensaladas} from "./ensaladas.js";
 import {modal} from "./modal.js";
 
+let principalDesayuno = document.querySelector('.principalDesayuno');
+let principalCafeteria = document.querySelector('.principalCafeteria');
+let principalBebidas = document.querySelector('.principalBebidas');
+let principalPlatos = document.querySelector('.principalPlatos');
+let principalEnsaladas = document.querySelector('.principalEnsaladas');
 
+
+
+let uno = document.querySelector('.uno');
+uno.addEventListener('click', () => {
+    principalCafeteria.classList.add("hidden");
+    principalDesayuno.classList.remove("hidden");
+    principalBebidas.classList.add("hidden");
+    principalPlatos.classList.add("hidden");
+    principalEnsaladas.classList.add("hidden");
+});
+
+let dos = document.querySelector('.dos');
+dos.addEventListener('click', () => {
+    principalCafeteria.classList.remove("hidden");
+    principalDesayuno.classList.add("hidden");
+    principalBebidas.classList.add("hidden");
+    principalPlatos.classList.add("hidden");
+    principalEnsaladas.classList.add("hidden");
+});
+let tres = document.querySelector('.tres');
+tres.addEventListener('click', () => {
+    principalCafeteria.classList.add("hidden");
+    principalDesayuno.classList.add("hidden");
+    principalBebidas.classList.remove("hidden");
+    principalPlatos.classList.add("hidden");
+    principalEnsaladas.classList.add("hidden");
+});
+
+let cuatro = document.querySelector('.cuatro');
+cuatro.addEventListener('click', () => {
+    principalCafeteria.classList.add("hidden");
+    principalDesayuno.classList.add("hidden");
+    principalBebidas.classList.add("hidden");
+    principalPlatos.classList.remove("hidden");
+    principalEnsaladas.classList.add("hidden");
+});
+
+let cinco = document.querySelector('.cinco');
+cinco.addEventListener('click', () => {
+    principalCafeteria.classList.add("hidden");
+    principalDesayuno.classList.add("hidden");
+    principalBebidas.classList.add("hidden");
+    principalPlatos.classList.add("hidden");
+    principalEnsaladas.classList.remove("hidden");
+});
 
 window.addEventListener('DOMContentLoaded', (e) => {
     desayunosMeriendas();
